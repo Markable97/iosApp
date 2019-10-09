6c033ec2b54b8754760a216f222d0879730d8913
Markable <markableglushko@gmail.com>
2019-10-09 19:57:25 +0300

    Таблица для календаря, Пока без дизайна

--- HelloWorld/CalendarController.swift
+++ HelloWorld/CalendarController.swift
@@ -8,14 +8,13 @@
 
 import UIKit
 
-class CalendarController: UIViewController {
-
-    @IBOutlet weak var textView: UITextView!
+class CalendarController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    
+    @IBOutlet weak var tableview: UITableView!
     @IBOutlet weak var indicator: UIActivityIndicatorView!
     
     @IBAction func onClickRefresh(){
         if !rvc.isDownloading{
-            textView.text = "TEEEEEEST"
             indicator.startAnimating()
             
             rvc.controlDownloading()
@@ -25,7 +24,7 @@ class CalendarController: UIViewController {
         }
     }
     var rvc: TabBarMainController!
-    var calendar: [NextMatch]!
+    var calendar = [NextMatch]()
     let decoder = JSONDecoder()
     
     override func viewDidLoad() {
@@ -41,18 +40,30 @@ class CalendarController: UIViewController {
         //print("Данные пришли в Calendar \(data)")
         indicator.stopAnimating()
         if data == "ERROR"{
-            textView.text = data
             present(AlertVisible.showAlert(message: "Ошибка сети"), animated: true, completion: nil)
         }else{
-           self.textView.text = data
+           //self.textView.text = data
             guard let calendar = try? decoder.decode([NextMatch].self, from: data.data(using: .utf8)!) else {
                 print("CalendarConroler bad JSON decode")
                 return
             }
             self.calendar = calendar
+            tableview.reloadData()
         }
     }
 
+    // MARK: - TABLE
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        calendar.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        let calendar = self.calendar[row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = calendar.teamHome!+" - "+calendar.teamVisit!
+        return cell!
+    }
     /*
     // MARK: - Navigation
 
