81029ddf8d0f6bfee208aa3d089fe1705d79a360
Markable <markableglushko@gmail.com>
2019-10-20 12:38:41 +0300

    Добавил разметку для всех матче + переход на детальный экран матча

--- HelloWorld/MatchesController.swift
+++ HelloWorld/MatchesController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class MatchesController: UIViewController {
+class MatchesController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
+    var results = [PrevMatch]()
+    
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Matches VC did load")
@@ -18,6 +22,67 @@ class MatchesController: UIViewController {
     
     func sendFromDownVC(data: String){
         print("This MatchesVC \(data.count)")
+        let decoder = JSONDecoder()
+        guard let results = try? decoder.decode([PrevMatch].self, from: data.data(using: .utf8)!) else {
+            print("ResultConroler bad JSON decoder")
+            return
+        }
+        self.results = results
+        self.tableview.reloadData()
+    }
+    // MARK: - Table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! ResultCell
+        let result: PrevMatch = self.results[row]
+        cell.tour.text = "Тур " + String(result.idTour!)
+        //IMAGE HOME
+        if !result.imageHome!.isEmpty{
+            let decodeData = NSData(base64Encoded: result.imageHome!, options: .ignoreUnknownCharacters)!
+            let decodedimage = UIImage(data: decodeData as Data)
+            cell.imaageHome.image = decodedimage
+        }
+        //IMAGE GUEST
+        if !result.imageGuest!.isEmpty{
+            let decodeData = NSData(base64Encoded: result.imageGuest!, options: .ignoreUnknownCharacters)!
+            let decodedimage = UIImage(data: decodeData as Data)
+            cell.imageVisit.image = decodedimage
+        }
+
+        cell.teamHome.text = result.teamHome!
+        cell.teamVisit.text = result.teamVisit!
+        cell.goalHome.text = String(result.goalHome!)
+        cell.goalVisit.text = String(result.goalVisit!)
+        
+        return cell
+    }
+
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "MatchDetail"{
+            if let indexPath = self.tableview.indexPathForSelectedRow{
+                let matchDetail = segue.destination as! MatchController
+                matchDetail.division = results[indexPath.row].nameDivision!
+                matchDetail.tour = results[indexPath.row].idTour!
+                let goalHome = results[indexPath.row].goalHome!
+                let goalGuest = results[indexPath.row].goalVisit!
+                matchDetail.score = String(goalHome) + " : " + String(goalGuest)
+                matchDetail.teamHome = results[indexPath.row].teamHome!
+                matchDetail.teamGuest = results[indexPath.row].teamVisit!
+                matchDetail.imHomeBase64 = results[indexPath.row].imageHome!
+                matchDetail.imGuestBase64 = results[indexPath.row].imageGuest!
+                matchDetail.idMatch = results[indexPath.row].id_match!
+            }
+        }
     }
     /*
     // MARK: - Navigation
